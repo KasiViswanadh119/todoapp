8e33ee796cd183b7f8970cc88052cf91dfaffd13
Kasi Viswanadh Duvva <21mh5a4916@acoe.edu.in>
2024-03-22 13:34:49 +0530

    Newchanges

--- src/components/TaskEditForm.js
+++ src/components/TaskEditForm.js
@@ -72,6 +72,7 @@ const TaskEditForm = ({ onSubmit, onCancel, initialData }) => {
             <div className='formcenterstatus'>
               <div>
                 <label htmlFor="status">Status:</label>
+                <span></span>
               </div>
               <select
                 id="status"
